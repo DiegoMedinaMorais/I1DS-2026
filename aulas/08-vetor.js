89ab30d8c89aed295781c54bf02924fa95c4d14c
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-03-17 14:07:11 -0300

    vetor v2

--- aulas/08-vetor.js
+++ aulas/08-vetor.js
@@ -23,6 +23,49 @@ console.log("O vetor agora tem", dinos.length, "elementos.");
 
 // obter um elemento a partir do index
 
-console.log("1ª posição:", dinos[0])
-console.log("4ª posição:", dinos[3])
-console.log("20ª posição (não existe):", dinos[19])
+console.log("1ª posição:", dinos[0]);
+console.log("4ª posição:", dinos[3]);
+console.log("20ª posição (não existe):", dinos[19]);
+
+// pop - remove um elemento do final do vetor (último elemento)
+dinos.pop();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos.");
+
+// shift - remove um elemento do início do vetor (primeiro elemento)
+dinos.shift();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos.");
+
+// splice - remove um elemento a partir de um index, e também precisa ser informada a quantidade de registro a ser excluído a partir do index
+dinos.splice(1, 1); // remove somente o segundo item da lista
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos.");
+
+dinos.push("Pterodáctilo");
+dinos.push("Estegossauro");
+dinos.push("Argentinossauro");
+
+// procurar um elemento a partir de um nome
+// indexOf(procurado) - retorna o index do elemento no array ou -1 caso não encontre o dinossauro
+let procurado = "Estegossauro";
+let index = dinos.indexOf(procurado);
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos.");
+console.log("O", procurado, "está no index", index);
+
+procurado = "Cristianossauro";
+index = dinos.indexOf(procurado);
+console.log("O", procurado, "está no index", index, "(não encontrado)");
+
+// excluir um elemento com base em seu nome
+
+procurado = "Estegossauro";
+index = dinos.indexOf(procurado);
+console.log("O", procurado, "está no index", index, "pronto para exclusão.");
+dinos.splice(index, 1);
+
+// alterar o valor de um elemento a partir do index
+dinos[1] = "Carnotauro";
+console.table(dinos);
+
