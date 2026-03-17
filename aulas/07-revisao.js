4f2482b82c4e5bd1db5f6777fbbfe050519fc5a5
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-03-17 09:27:18 -0300

    arquivo revisão adicionada

---
+++ aulas/07-revisao.js
@@ -0,0 +1,57 @@
+// comentário em linha
+
+/*
+comentário
+de várias
+linhas
+*/
+
+console.log("Imprime informações na tela")
+// escrever clg e dar enter completa com console.log()
+
+/*
+variáveis:
+
+sintaxe: let tipo-variavel = valor-variavel;
+exemplo: let idade = 45;
+*/
+
+/*
+tipos dados primários:
+
+números inteiros (int) = 10
+números decimais (double) = 8.53
+caractéres alfanúmeros (string) = "texto"
+números booleanos (bool) = false;
+*/
+let idade = 45;
+let salario = 25864.57;
+let eProfessor = false;
+let nome = "João da Silva";
+
+console.log("Nome:", nome, "Idade:", idade, "Salário:", salario);
+
+/*
+operadores matemáticos:
+
++ - adição
+- - subtração
+* - multiplicação
+/ - divisão
+% - resto da divisão
+*/
+
+let num1 = 10;
+let num2 = 7
+
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+let restoDaDivisao = num1 % num2
+
+
+// imprimindo as operações
+console.log("Somando", num1, "+", num2, "=", soma)
+console.log("Resto da divisão do", num1, "por", num2, "=", restoDaDivisao)
+
