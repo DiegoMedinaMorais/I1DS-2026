f97de93369309b76839eb533e424ec34841e8010
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-03-17 10:40:58 -0300

    revisão v2

--- aulas/07-revisao.js
+++ aulas/07-revisao.js
@@ -6,7 +6,7 @@ de várias
 linhas
 */
 
-console.log("Imprime informações na tela")
+console.log("Imprime informações na tela");
 // escrever clg e dar enter completa com console.log()
 
 /*
@@ -40,18 +40,59 @@ operadores matemáticos:
 / - divisão
 % - resto da divisão
 */
-
 let num1 = 10;
-let num2 = 7
-
-let soma = num1 + num2
-let subtracao = num1 - num2
-let multiplicacao = num1 * num2
-let divisao = num1 / num2
-let restoDaDivisao = num1 % num2
-
+let num2 = 7;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let restoDaDivisao = num1 % num2;
 
 // imprimindo as operações
-console.log("Somando", num1, "+", num2, "=", soma)
-console.log("Resto da divisão do", num1, "por", num2, "=", restoDaDivisao)
+console.log("Somando", num1, "+", num2, "=", soma);
+console.log("Resto da divisão do", num1, "por", num2, "=", restoDaDivisao);
 
+/*
+operadores de comparação
+
+== - comparação de igualdade
+!= - diferente
+> - maior que
+< - menor que
+>= - maior ou igual
+<= - menor ou igual
+=== - exatamente igual (valor e tipo de variável)
+
+retorna true ou false 
+*/
+console.log("O número 1 é igual ao número 2?", num1 == num2);
+console.log("O número 1 é maior ou igual ao número 2?", num1 >= num2);
+console.log("O número 1 é menor que o número 2?", num1 < num2);
+
+/*
+operadores lógicos
+
+&& (AND) - retorna true ou false se todas as clausulas forem verdadeiras
+|| (OR) - retorna true ou false se uma das clausulas forem verdadeiras
+! (NOT) - operador de negação (inverte o valor lógico)
+*/
+let sexo = "Feminino";
+let idadeAluno = 17;
+
+console.log(
+  "É sexo masculino ou tem 17 ou mais anos de idade?",
+  sexo == "Masculino" || idadeAluno >= 17,
+);
+
+// concatenando strings
+
+let n1 = "20";
+let n2 = 10;
+
+console.log(n1+n2);
+console.log("20"+n2);
+
+let nome1 = "Cristiano";
+let nome2 = " de Paula";
+
+console.log(nome1+nome2);
