a06101a2465b523a0a74e9e436b8c52711674372
DiegoMedinaMorais <diegomedinamorais2@gmail.com>
2026-05-12 13:35:55 -0300

    v41

--- atividades (html)/03-atividadeMiniSistema/js/scripts.js
+++ atividades (html)/03-atividadeMiniSistema/js/scripts.js
@@ -1,54 +1,119 @@
-let n = 0
+let n = 0;
 let infoEmail = [];
 let infoNome = [];
 
 const remove = (id) => {
-    let lista = document.getElementById("lista")
-    let elemento = document.getElementById(id)
-    let contagem = lista.childElementCount
-    
-    lista.removeChild(elemento)
+  let lista = document.getElementById("lista");
+  let elemento = document.getElementById(id);
+  let contagem = lista.childElementCount;
 
-    document.getElementById("contagem").innerText = contagem
-}
+  lista.removeChild(elemento);
+
+  document.getElementById("contagem").innerText = contagem - 1;
+};
 
 const edit = (id) => {
-    let nome = document.getElementById("nome")
-    let email = document.getElementById("email")
+  let nome = document.getElementById("nome");
+  let email = document.getElementById("email");
 
-    nome.value = infoNome[id-1]
-    email.value = infoEmail[id-1]
-}
+  nome.value = infoNome[id - 1];
+  email.value = infoEmail[id - 1];
+};
 
 const cadastrar = () => {
-    // parar a propagaão padrão do evento
-    event.preventDefault();
+  // parar a propagaão padrão do evento
+  event.preventDefault();
 
-    // capturando  os valores dos campos dos elementos por id
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
+  // capturando  os valores dos campos dos elementos por id
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
 
-    // capturando o elemento de lista por id
-    let lista = document.getElementById("lista");
+  // capturando o elemento de lista por id
+  let lista = document.getElementById("lista");
 
-    // criar um novo elemento <li>Nome - Email</li>
-    let novoItem = document.createElement("li");
-    n++
-    let id = n
-    novoItem.innerHTML = id + " - " + nome + " - " + email + '<button onclick="remove('+ id +')" class="removeButton">Remover</button>' + '<button onclick="edit('+ id +')" class="editButton">Editar</button>';
-    novoItem.id = id
+  // criar um novo elemento <li>Nome - Email</li>
+  let novoItem = document.createElement("li");
+  n++;
+  let id = n;
+  novoItem.innerHTML =
+    id +
+    " - " +
+    nome +
+    " - " +
+    email +
+    '<button onclick="remove(' +
+    id +
+    ')" class="removeButton">Remover</button>' +
+    '<button onclick="edit(' +
+    id +
+    ')" class="editButton">Editar</button>';
+  novoItem.id = id;
 
-    infoEmail.push(email)
-    infoNome.push(nome)
+  infoEmail.push(email);
+  infoNome.push(nome);
 
-    // adicionar o novo item na lista existente
-    lista.appendChild(novoItem);
+  // adicionar o novo item na lista existente
+  lista.appendChild(novoItem);
 
-    // limpar os campos
-    document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
+  // limpar os campos
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
 
-    let contagem = lista.childElementCount
-    document.getElementById("contagem").innerText = contagem
-}
+  let contagem = lista.childElementCount;
+  document.getElementById("contagem").innerText = contagem;
+};
 
+let n2 = 0;
+let soma = 0;
+
+const adicionarProduto = () => {
+  let prod = document.getElementById("produto");
+  let qtd = document.getElementById("quantidade");
+  let val = document.getElementById("valor");
+  let n3 = n2 + "a"; // adicionei "a" no final do número (que será o id) pois já usei id com somente número nas funções acima.
+
+  let table = document.getElementById("tabelaProdutos");
+  let tbody = table.lastElementChild;
+
+  tbody.innerHTML +=
+    "<tr><td>" +
+    prod.value +
+    "</td><td>" +
+    qtd.value +
+    "</td><td>" +
+    val.value +
+    "</td><td id='" +
+    n3 +
+    "'>" +
+    val.value * qtd.value +
+    "</td></tr>";
+  n2 += 1;
+};
+
+const finalizarCompra = () => {
+  soma = 0;
+  let total = document.getElementById("total");
+
+  total.innerHTML = document;
+  for (let i = 0; i < n2; i++) {
+    let a = document.getElementById(i + "a");
+    soma += Number(a.innerHTML);
+  }
+
+  total.innerHTML = soma.toFixed(2);
+  alert("Compra realizada com sucesso")
+};
+
+const aplicarDesconto = () => {
+  let descReais = document.getElementById("descontoValor");
+  let descPorc = document.getElementById("descontoPercentual");
+  let valorLiq = document.getElementById("valorLiquido");
+
+  if (descPorc.value != "" && descReais.value != "") {
+    alert("Somente uma opção de desconto é permitida")
+  } else if (descPorc.value != "") {
+    valorLiq.innerHTML = (soma - (descPorc.value / 100) * soma).toFixed(2);
+  } else if (descReais.value != "") {
+    valorLiq.innerHTML = (soma - descReais.value).toFixed(2);
+  }
+};
--- atividades (html)/06-4atividades/js/scripts.js
+++ atividades (html)/06-4atividades/js/scripts.js
@@ -20,15 +20,17 @@ const gerarImagem = () => {
   let quant = document.getElementById("inQtdImg").value;
 
   for (let i = 0; i < quant; i++) {
-    div.innerHTML += '<img src="img/logo.png" alt="Logotipo SENAI" class="imagens">';
+    div.innerHTML +=
+      '<img src="img/logo.png" alt="Logotipo SENAI" class="imagens">';
   }
 };
 // -----------------------------------------
 
 //ATIVIDADE 03
 const validaForm = () => {
+  event.preventDefault();
 
-event.preventDefault()
+  /*
 
   let inData = document.getElementById("inData").value;
   let inCli = document.getElementById("inCli").value;
@@ -48,11 +50,100 @@ event.preventDefault()
     inQtd == 0 ||
     inVal == 0
   ) {
+    erro.innerText = "Preencha todos os campos";
     erro.style.display = "block";
-    erro.innerText += "Preencha todos os campos";
+  } else if (
+    inCli.length < 5 ||
+    inFone.length < 5 ||
+    inMail.length < 5 ||
+    inProd.length < 5
+  ) {
+    erro.innerText = "Preencha os campos com mais de 5 letras";
+    erro.style.display = "block";
+  } else if (
+    inQtd < 0 ||
+    inVal < 0
+  ) {
+    erro.innerText = "Use valores positivos";
+    erro.style.display = "block";
+  } else {
+    erro.innerText = "";
+    erro.style.display = "";
+  }
+
+  */
+
+  // do professor
+
+  let erro = document.getElementById("mensagem-erro");
+  erro.innerHTML = "";
+
+  let data = inData; // document.getElementByName("inData");
+  let nome = inCli; // pega elemento por name
+  let fone = inFone;
+  let mail = inMail;
+  let prod = inProd;
+  let qtd = inQtd;
+  let val = inVal;
+
+  switch (true) {
+    // Verificar se existe algum campo vazio
+    // trim tira espaços desnecessários
+    case nome.value.trim() == "" ||
+      data.value == "" ||
+      fone.value.trim() == "" ||
+      mail.value.trim() == "" ||
+      prod.value.trim() == "" ||
+      qtd.value == "" ||
+      val.value == "":
+      erro.style.display = "block";
+      erro.innerHTML = "Erro: O(s) campo(s) não pode(m) estar vazio(s)!";
+      break;
+
+    // Verificar se os campos de string contem mais que 5 caracteres
+    case nome.value.trim().length < 5 ||
+      prod.value.trim().length < 5 ||
+      mail.value.trim().length < 5 ||
+      fone.value.trim().length < 5:
+      erro.innerHTML = "O campo texto não pode ter menos que 5 caracteres!";
+      erro.style.display = "block";
+      break;
+
+    // Verificar se a quantidade e o valor são positivos (> 0)
+    case val.value < 0 || qtd.value < 0:
+      erro.style.display = "block";
+      erro.innerHTML = "Erro: Este campo não pode ser negativo!";
+      break;
+
+    default:
+      erro.style.display = "none";
+      break;
   }
 };
 // -----------------------------------------
 
 // ATIVIDADE 04
-const calcular = () => {};
+const calcular = () => {
+  let valPedido = Number(inValorPedido.value);
+  let perDesconto = 0;
+  let valDesconto = 0;
+  let valFinalPedido = 0;
+
+  if (valPedido >= 2000) {
+    perDesconto = 1.5;
+  } else if (valPedido >= 1500) {
+    perDesconto = 1;
+  } else if (valPedido >= 1000) {
+    perDesconto = 0.8;
+  } else if (valPedido >= 500) {
+    perDesconto = 0.5;
+  } else {
+    perDesconto = 0;
+  }
+
+  valDesconto = (valPedido * perDesconto) / 100;
+  valFinalPedido = valPedido - valDesconto;
+  inPercDesc.value = perDesconto;
+  inValDesc.value = valDesconto.toFixed(2); // o mesmo que documento:getElement... so funciona para campos de form (name)
+  inValFinal.value = valFinalPedido.toFixed(2);
+};
